e759397e1c5ad320764d46934b69bca45a1dfbec
labocho <labocho@penguinlab.jp>
2021-02-26 10:43:48 +0900

    bundle gem rack-html_validator

---
+++ lib/rack/html_validator/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module HtmlValidator
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/rack/html_validator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rack::HtmlValidator do
+  it "has a version number" do
+    expect(Rack::HtmlValidator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
